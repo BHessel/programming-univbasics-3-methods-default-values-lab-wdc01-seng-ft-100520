617645288369ec5d97bac691dedc2fd741206b23
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-11 13:37:20 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 def meal_choice (veg1, veg2, protein = "tofu")
   puts "What a nutritious meal"
   puts "A place of #{protein} with #{veg1} and #{veg2}"
-  return ()
+  meal_choice
 end
 
 meal_choice ("carrots", "greens", "chicken")
